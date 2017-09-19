4683048ba886a9a6b4cf2958bd4d0bd18e55c290
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-19 10:58:18 -0700

    added application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :authenticate_token, except: [:login, :create]
-  before_action :authorize_user, except: [:login, :create, :index]
+  # before_action :authenticate_token, except: [:login, :create]
+  # before_action :authorize_user, except: [:login, :create, :index]
 
   def login
     user = User.find_by(username: params[:user][:username])
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
